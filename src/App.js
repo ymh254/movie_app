0a3e95253ff55a874658565a26b72d99bb70f5a9
ymh254 <ymh254@gmail.com>
2020-12-01 23:11:23 +0900

    add genres

--- src/App.js
+++ src/App.js
@@ -23,13 +23,13 @@ class App extends React.Component {
   render() {
     const { isLoading, movies } = this.state;
     return (
-      <section class="container">
+      <section className="container">
         {isLoading ? (
-          <div class="loader">
-            <span class="loader_text">Loading...</span>
+          <div className="loader">
+            <span className="loader_text">Loading...</span>
           </div>
         ) : (
-            <div class="movie">
+            <div className="movie">
               {movies.map(movie => (
                 <Movie
                   key={movie.id}
@@ -38,6 +38,7 @@ class App extends React.Component {
                   title={movie.title}
                   summary={movie.summary}
                   poster={movie.medium_cover_image}
+                  genres={movie.genres}
                 />))}
             </div>
           )}
--- src/Movie.js
+++ src/Movie.js
@@ -1,13 +1,20 @@
 import React from "react";
 
-function Movie({ year, title, summary, poster }) {
+function Movie({ year, title, summary, poster, genres }) {
     return (
-        <div class="movie">
+        <div className="movie">
             <img src={poster} alt={title} title={title} />
-            <div class="movie_data">
-                <h3 class="movie_title">{title}</h3>
-                <h5 class="movie_year">{year}</h5>
-                <p class="movie_summary">{summary}</p>
+            <div className="movie_data">
+                <h3 className="movie_title">{title}</h3>
+                <h5 className="movie_year">{year}</h5>
+                <ul className="genres">
+                    {genres.map((genre, index) => (
+                        <li key={index} className="genre">
+                            {genre}
+                        </li>
+                    ))}
+                </ul>
+                <p className="movie_summary">{summary}</p>
             </div>
         </div >
     )
